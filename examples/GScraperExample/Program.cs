2855bddb6f3a61917e1a0def7acdd4c4a5ac1448
PsykoDev <johann.l_62@hotmail.fr>
2022-12-26 10:52:02 +0100

    clean up codee

--- examples/GScraperExample/Program.cs
+++ examples/GScraperExample/Program.cs
@@ -2,7 +2,6 @@
 using GScraper.DuckDuckGo;
 using GScraper.Google;
 using GScraperExample.function;
-using HtmlAgilityPack;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
@@ -16,16 +15,17 @@ namespace GScraperExample;
 
 internal static class Program
 {
-    static ConnectionMultiplexer redis;
-    static Queue<string> qword;
+    private static ConnectionMultiplexer? redis;
+    private static Queue<string>? qword;
 
     [DllImport("Kernel32")]
     private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
 
     private delegate bool EventHandler(CtrlType sig);
-    static EventHandler _handler;
 
-    enum CtrlType
+    private static EventHandler? _handler;
+
+    private enum CtrlType
     {
         CTRL_C_EVENT = 0,
         CTRL_BREAK_EVENT = 1,
@@ -41,11 +41,11 @@ internal static class Program
 
         while (qword.Count != 0)
         {
-            redis.GetDatabase().ListLeftPush("words_list", qword.Dequeue());
+            _ = redis.GetDatabase().ListLeftPush("words_list", qword.Dequeue());
         }
 
         Console.WriteLine("All done press enter to exit");
-        Console.ReadLine();
+        _ = Console.ReadLine();
         Environment.Exit(-1);
         return true;
     }
@@ -56,7 +56,7 @@ internal static class Program
         qword = new();
 
         _handler += new EventHandler(HandlerAsync);
-        SetConsoleCtrlHandler(_handler, true);
+        _ = SetConsoleCtrlHandler(_handler, true);
 
         Queue<string> word = new();
 
@@ -67,9 +67,9 @@ internal static class Program
         //    word.Enqueue(s);
         //}
 
-        var opts = new Uri(args[0]);
-        var credentials = opts.UserInfo.Split(':');
-        var options = ConfigurationOptions.Parse($"{opts.Host}:{opts.Port},password={credentials[1]},user={credentials[0]}");
+        Uri opts = new(args[0]);
+        string[] credentials = opts.UserInfo.Split(':');
+        ConfigurationOptions options = ConfigurationOptions.Parse($"{opts.Host}:{opts.Port},password={credentials[1]},user={credentials[0]}");
         options.ReconnectRetryPolicy = new ExponentialRetry(10);
         options.CommandMap = CommandMap.Create(new HashSet<string> { "SUBSCRIBE" }, false);
         redis = ConnectionMultiplexer.Connect(options);
@@ -78,27 +78,20 @@ internal static class Program
 
         IDatabase conn = redis.GetDatabase();
 
-        using var scraper = new GoogleScraper();
-        using var duck = new DuckDuckGoScraper();
-        using var brave = new BraveScraper();
-        HtmlNodeCollection table;
+        using GoogleScraper scraper = new();
+        using DuckDuckGoScraper duck = new();
+        using BraveScraper brave = new();
 
 
 
-        var key = new RedisKey("already_done_list");
-        var meow = await conn.ListGetByIndexAsync(key, 0);
+        RedisKey key = new("already_done_list");
+        RedisValue meow = await conn.ListGetByIndexAsync(key, 0);
         string text = meow.ToString();
         qword.Enqueue(text);
 
         long totalimageupload = 0;
 
-        double waittime;
-        if (args.Length > 0.1)
-            waittime = double.Parse(args[1]);
-        else
-            waittime = 0;
-
-
+        double waittime = args.Length > 0.1 ? double.Parse(args[1]) : 0;
         if (redis.IsConnected)
         {
 
@@ -119,21 +112,23 @@ internal static class Program
             {
                 timer.Start();
 
-                var site = await searchEngineRequest.getAllDataFromsearchEngineAsync(text);
+                Dictionary<string, IEnumerable<GScraper.IImageResult>> site = await searchEngineRequest.getAllDataFromsearchEngineAsync(text);
 
-                var callQword = await searchEngineRequest.getAllNextTag(text, redis);
+                Queue<string> callQword = await searchEngineRequest.getAllNextTag(text, redis);
 
                 while (callQword.Count != 0)
+                {
                     qword.Enqueue(callQword.Dequeue()); // euh
+                }
 
-                var tr = await redisImagePush.GetAllImageAndPush(conn, site);
+                long tr = await redisImagePush.GetAllImageAndPush(conn, site);
                 totalimageupload += tr;
 
                 site.Clear();
 
                 if (qword.Count <= 2)
                 {
-                    var newword = await getNewtag(redis);
+                    RedisValue newword = await getNewtag(redis);
                     qword.Enqueue(newword.ToString());
                     text = qword.Dequeue();
                     Console.ForegroundColor = ConsoleColor.Magenta;
@@ -150,7 +145,7 @@ internal static class Program
                     Console.ForegroundColor = ConsoleColor.Red;
                     await Console.Out.WriteLineAsync("redis disconnected, press enter to stop");
                     Console.ResetColor();
-                    Console.ReadLine();
+                    _ = Console.ReadLine();
                     break;
                 }
 
@@ -161,7 +156,7 @@ internal static class Program
                         Console.ForegroundColor = ConsoleColor.Red;
                         await Console.Out.WriteLineAsync($"Redis queue alomst full {conn.ListLength("image_jobs")}");
                         Console.ResetColor();
-                        Console.ReadLine();
+                        _ = Console.ReadLine();
                     }
                     catch { }
                 }
@@ -173,7 +168,7 @@ internal static class Program
                 catch { }
 
                 timer.Stop();
-                var uptime2 = $"{uptime.Elapsed.Days} days {uptime.Elapsed.Hours.ToString("00")}:{uptime.Elapsed.Minutes.ToString("00")}:{uptime.Elapsed.Seconds.ToString("00")}";
+                string uptime2 = $"{uptime.Elapsed.Days} days {uptime.Elapsed.Hours:00}:{uptime.Elapsed.Minutes:00}:{uptime.Elapsed.Seconds:00}";
                 await Console.Out.WriteLineAsync("================================================================================================================================");
 
                 try
@@ -183,7 +178,7 @@ internal static class Program
                          $"Done in\t\t{timer.ElapsedMilliseconds} ms\n" +
                          $"Previous\t{text}\nNext\t\t{qword.ToArray()[qword.Count - 1]}\nT" +
                          $"ags\t\t{qword.Count}\n" +
-                         $"Redis Length\t{conn.SetLength("image_jobs")} / {uint.MaxValue} ({(100.0 * (float)conn.SetLength("image_jobs") / (float)uint.MaxValue).ToString("0.000")}%)\n" +
+                         $"Redis Length\t{conn.SetLength("image_jobs")} / {uint.MaxValue} ({100.0 * conn.SetLength("image_jobs") / uint.MaxValue:0.000}%)\n" +
                          $"Words Length\t{await redis.GetDatabase().ListLengthAsync(key)}\n" +
                          $"Total upload\t{totalimageupload}\n" +
                          $"Sleep\t\t{waittime} sec");
@@ -195,7 +190,7 @@ internal static class Program
                         $"Done in\t\t{timer.ElapsedMilliseconds} ms\n" +
                         $"Previous\t{text}\n" +
                         $"Tags\t\t{qword.Count}\n" +
-                        $"Redis Length\t{conn.SetLength("image_jobs")} / {uint.MaxValue} ({(100.0 * (float)conn.SetLength("image_jobs") / (float)uint.MaxValue).ToString("0.000")}%)\n" +
+                        $"Redis Length\t{conn.SetLength("image_jobs")} / {uint.MaxValue} ({100.0 * conn.SetLength("image_jobs") / uint.MaxValue:0.000}%)\n" +
                         $"Words Length\t{await redis.GetDatabase().ListLengthAsync(key)}\n" +
                         $"Total upload\t{totalimageupload}\n" +
                         $"Sleep\t\t{waittime} sec");
@@ -207,12 +202,15 @@ internal static class Program
         }
     }
 
-    private static async Task<RedisValue> getNewtag(ConnectionMultiplexer redis) => await redis.GetDatabase().ListLeftPopAsync("words_list");
+    private static async Task<RedisValue> getNewtag(ConnectionMultiplexer redis)
+    {
+        return await redis.GetDatabase().ListLeftPopAsync("words_list");
+    }
 
     private static async void write(string text, ConnectionMultiplexer redis)
     {
-        var value = new RedisValue(text);
-        var key = new RedisKey("already_done_list");
-        await redis.GetDatabase().ListLeftPushAsync(key, value);
+        RedisValue value = new(text);
+        RedisKey key = new("already_done_list");
+        _ = await redis.GetDatabase().ListLeftPushAsync(key, value);
     }
 }
