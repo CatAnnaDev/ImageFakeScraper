008e4a40cdff81c6d8c4e0e9325fe34164154d2a
PsykoDev <johann.l_62@hotmail.fr>
2023-01-03 10:26:54 +0100

    tmp

--- src/GScraper/Pixel/PixelScraper.cs
+++ src/GScraper/Pixel/PixelScraper.cs
@@ -62,7 +62,7 @@ namespace GScraper.Pixel
                     NbOfRequest++;
                 }
             }
-            catch (Exception e) { Console.WriteLine(e); }
+            catch (Exception e) { }
             return tmp;
         }
     }
